31425fb54716ee92d09fe8f05a47dce71119bf29
jengwork <zyjeng@light-year.tech>
2022-01-25 19:41:18 +0800

    project_init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComicomiCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
